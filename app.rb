7777d41dc93d43cfe9accb1e66d29760821f0227
Vimal V Nair <vimalvnair999@gmail.com>
2018-02-18 17:50:14 +0530

    remove db host

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ require_relative './idea_usage_fetcher'
 include IdeaCellular
 
 configure :production do
-  set :database, {adapter: 'postgresql',  encoding: 'unicode', database: ENV['DB_NAME'], pool: 2, username: ENV['DB_USER'], password: ENV['DB_PASSWORD'], host: ENV['DB_HOST'], port: ENV['DB_PORT']}
+  set :database, {adapter: 'postgresql',  encoding: 'unicode', database: ENV['DB_NAME'], pool: 2, username: ENV['DB_USER'], password: ENV['DB_PASSWORD'], port: ENV['DB_PORT']}
 end
 
 configure :development do
